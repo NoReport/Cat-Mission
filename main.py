fe58710b8e1d9ceceaf8e944d4ea30e70f7f4dd8
นรภัทร หนูทอง <noorrapat1247@gmail.com>
2024-02-20 16:33:50 +0700

    create sprite

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
-from kivy.properties import NumericProperty, ReferenceListProperty
+from kivy.properties import NumericProperty, ReferenceListProperty, ObjectProperty
 from kivy.vector import Vector
+from kivy.clock import Clock
+from random import randint
 
 
-class Sprite(Widget):
-    x, y = 0, 0
+class Cat(Widget):
     velocity_x = NumericProperty(0)
     velocity_y = NumericProperty(0)
     velocity = ReferenceListProperty(velocity_x, velocity_y)
@@ -15,12 +16,24 @@ class Sprite(Widget):
 
 
 class CatQuest(Widget):
-    pass
+    cat = ObjectProperty(None)
+
+    def spawn_cat(self):
+        self.cat = Cat()
+        self.cat.center = self.center
+        self.cat.velocity = Vector(4, 0).rotate(randint(0, 360))
+        self.add_widget(self.cat)
+
+    def update(self, dt):
+        self.cat.move()
 
 
 class CatApp(App):
     def build(self):
-        return CatQuest()
+        game = CatQuest()
+        game.spawn_cat()
+        Clock.schedule_interval(game.update, 1.0 / 60.0)
+        return game
 
 
 if __name__ == "__main__":
