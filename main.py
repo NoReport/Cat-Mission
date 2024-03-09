272824194b8e95f1b6f2dd6f6a4608bc70095713
6610110143 <6610110143@psu.ac.th>
2024-03-09 16:57:43 +0700

    create cat class

--- main.py
+++ main.py
@@ -7,33 +7,58 @@ from random import randint
 
 
 class Cat(Widget):
-    velocity_x = NumericProperty(0)
-    velocity_y = NumericProperty(0)
-    velocity = ReferenceListProperty(velocity_x, velocity_y)
+    health = NumericProperty(100)
 
     def move(self):
-        self.pos = Vector(*self.velocity) + self.pos
+        self.pos = Vector(*self.velocity) + self.pos    
+
+
+class Enemy(Widget):
+    pass  # Enemy properties and methods can be added if needed
 
 
 class CatQuest(Widget):
     cat = ObjectProperty(None)
+    enemy = ObjectProperty(None)
 
-    def spawn_cat(self):
-        self.cat = Cat()
-        self.cat.center = self.center
-        self.cat.velocity = Vector(4, 0).rotate(randint(0, 360))
-        self.add_widget(self.cat)
+    def __init__(self, **kwargs):
+        super(CatQuest, self).__init__(**kwargs)
+        self.cat.pos = self.width / 2 - 25, self.height / 2 - 25
+        self.enemy.pos = randint(0, self.width - 50), randint(0, self.height - 50)
+        self.cat.velocity = Vector(0, 0)
+        self.enemy.velocity = Vector(4, 4)
+        Clock.schedule_interval(self.update, 1.0 / 60.0)
 
     def update(self, dt):
         self.cat.move()
 
+        if self.cat.collide_widget(self.enemy):
+            self.cat.health -= 10
+
+        if self.cat.health <= 0:
+            print("You have been defeated by the enemy!")
+            self.reset_game()
+
+        if self.cat.right > self.width:
+            self.cat.right = self.width
+        if self.cat.x < 0:
+            self.cat.x = 0
+        if self.cat.top > self.height:
+            self.cat.top = self.height
+        if self.cat.y < 0:
+            self.cat.y = 0
+
+    def reset_game(self):
+        # Reset the game state here
+        pass
+
+    def on_touch_move(self, touch):
+        self.cat.velocity = Vector(touch.x - self.cat.center_x, touch.y - self.cat.center_y)
+
 
 class CatApp(App):
     def build(self):
-        game = CatQuest()
-        game.spawn_cat()
-        Clock.schedule_interval(game.update, 1.0 / 60.0)
-        return game
+        return CatQuest()
 
 
 if __name__ == "__main__":
