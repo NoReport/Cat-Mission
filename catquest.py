7d0208e637827e96ebb292db7e268761187c4f4b
6610110143 <6610110143@psu.ac.th>
2024-03-16 17:55:42 +0700

    create cat and enemy

--- catquest.py
+++ catquest.py
@@ -1,21 +1,23 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
-from kivy.properties import NumericProperty, ReferenceListProperty, ObjectProperty
+from kivy.properties import NumericProperty, ObjectProperty
 from kivy.vector import Vector
 from kivy.clock import Clock
 from random import randint
 
 
 class Cat(Widget):
-    def __init__(self, **kwrag):
-        health = NumericProperty(100)
+    health = NumericProperty(100)
 
     def move(self):
         self.pos = Vector(*self.velocity) + self.pos    
 
 
 class Enemy(Widget):
-    pass  # Enemy properties and methods can be added if needed
+    health = NumericProperty(50)
+
+    def move(self):
+        self.pos = Vector(*self.velocity) + self.pos
 
 
 class CatQuest(Widget):
@@ -24,6 +26,8 @@ class CatQuest(Widget):
 
     def __init__(self, **kwargs):
         super(CatQuest, self).__init__(**kwargs)
+        self.cat = Cat()
+        self.enemy = Enemy()
         self.cat.pos = self.width / 2 - 25, self.height / 2 - 25
         self.enemy.pos = randint(0, self.width - 50), randint(0, self.height - 50)
         self.cat.velocity = Vector(0, 0)
@@ -32,6 +36,7 @@ class CatQuest(Widget):
 
     def update(self, dt):
         self.cat.move()
+        self.enemy.move()
 
         if self.cat.collide_widget(self.enemy):
             self.cat.health -= 10
@@ -49,9 +54,15 @@ class CatQuest(Widget):
         if self.cat.y < 0:
             self.cat.y = 0
 
+        if self.enemy.right > self.width or self.enemy.x < 0:
+            self.enemy.velocity_x *= -1
+        if self.enemy.top > self.height or self.enemy.y < 0:
+            self.enemy.velocity_y *= -1
+
     def reset_game(self):
-        # Reset the game state here
-        pass
+        self.cat.health = 100
+        self.cat.pos = self.width / 2 - 25, self.height / 2 - 25
+        self.enemy.pos = randint(0, self.width - 50), randint(0, self.height - 50)
 
     def on_touch_move(self, touch):
         self.cat.velocity = Vector(touch.x - self.cat.center_x, touch.y - self.cat.center_y)
@@ -59,9 +70,14 @@ class CatQuest(Widget):
 
 class CatApp(App):
     def build(self):
-        return CatQuest()
+        self.root = CatQuest()
+        Clock.schedule_once(self.start_game, 2)  # Delay the game start by 2 seconds
+        return self.root
+
+    def start_game(self, dt):
+        print("Starting the game!")
+        self.root.update(0)  # Start the game update loop
 
 
 if __name__ == "__main__":
     CatApp().run()
-
